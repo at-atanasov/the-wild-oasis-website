8c2872bdfa9332f53e0d95206555263895560e4f
at-atanasov <at.atanasov@gmail.com>
2026-02-23 22:38:46 +0200

    Fetching and Displaying Cabin List

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wmkkxiwadqfjadxfdtxf.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
