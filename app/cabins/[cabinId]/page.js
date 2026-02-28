fd14545238703a8d7fc6efeac89b101998a131d1
at-atanasov <at.atanasov@gmail.com>
2026-02-28 21:25:20 +0200

    Error Handling: "Not Found" Errors

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,19 +1,24 @@
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 //export const metadata = {
 //  title: "Cabin"
 //}
 
 export async function generateMetadata({ params }) {
-  const { name } = await getCabin(params.cabinId);
+  const { name } = getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
 }
 
 export default async function Page({ params }) {
-  const { cabinId } = await params;
+  const { cabinId } = params;
   const cabin = await getCabin(cabinId);
+
+  if (!cabin) {
+    notFound();
+  }
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
--- app/error.js
+++ app/error.js
@@ -6,7 +6,11 @@ export default function Error({ error, reset }) {
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
       <p className="text-lg">{error.message}</p>
 
-      <button className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg">
+      <button
+        type="button"
+        onClick={() => reset()}
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+      >
         Try again
       </button>
     </main>
