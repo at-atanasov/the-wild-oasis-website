4033d537b735a7032e41991a10bd7dfd2c9ba0de
at-atanasov <at.atanasov@gmail.com>
2026-03-16 22:51:15 +0200

    Reservation Date Selector and Form

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,5 +1,12 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
 import TextExpander from "@/app/_components/TextExpander";
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import {
+  getBookedDatesByCabinId,
+  getCabin,
+  getCabins,
+  getSettings,
+} from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -29,6 +36,9 @@ export default async function Page({ params }) {
   if (!cabin) {
     notFound();
   }
+
+  const settings = await getSettings();
+  const bookedDates = await getBookedDatesByCabinId(cabinId);
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
@@ -80,9 +90,14 @@ export default async function Page({ params }) {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-5xl font-semibold text-center  mb-10 text-accent-400">
+          Reserve {name} today. Pay on arrival.
         </h2>
+
+        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+          <DateSelector />
+          <ReservationForm />
+        </div>
       </div>
     </div>
   );
