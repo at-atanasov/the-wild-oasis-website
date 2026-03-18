a391c525fd011ef2bb2ec7f497a5c05830ade04f
at-atanasov <at.atanasov@gmail.com>
2026-03-18 20:44:49 +0200

    Using the Context API for State Management

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/navigation";
 
 import { Josefin_Sans } from "next/font/google";
 
@@ -10,6 +9,7 @@ const josefin = Josefin_Sans({
 
 import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 export const metadata = {
   //title: "The Wild Oasis",
@@ -29,7 +29,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid ">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
