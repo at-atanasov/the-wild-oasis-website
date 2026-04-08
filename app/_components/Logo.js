c2e6b34fd3d647f9ea4759c17fe22344fa1fdd0f
at-atanasov <at.atanasov@gmail.com>
2026-04-08 20:54:47 +0300

    Logo fix

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -6,7 +6,7 @@ function Logo() {
     <Link href="/" className="flex items-center gap-4 z-10">
       {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
       <Image
-        src="/Logo.png"
+        src="/logo.png"
         height="60"
         width="60"
         alt="The Wild Oasis logo"
